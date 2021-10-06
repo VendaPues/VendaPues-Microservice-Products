d32f9a5145b228ae824bf5e7803a48d62c10e13a
sf-burgos <brayanburgos1437@gmail.com>
2021-10-06 14:33:22 -0500

    creando la clase de testing

---
+++ src/test/java/ieti/protect/productTest/ProductTests.java
@@ -0,0 +1,13 @@
+package ieti.protect.productTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
